5393b95f45e45adb054a9bbee95746375542a9d4
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-07-01 13:28:29 +0500

    install react js and tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <Header>
+            <h1>
+                <span className="text-slate-400">Estate</span>
+            </h1>
+        </Header>
+    )
+}
+
+export default Header
