6482b3762f4b040a2e081fb005279e1f2a94b116
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-07-01 14:19:01 +0500

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
-
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 const Header = () => {
     return (
-        <Header>
-            <h1>
-                <span className="text-slate-400">Estate</span>
-            </h1>
-        </Header>
+        <header className="bg-slate-200 shadow-md ">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link>
+                    <h1 className="font-bold text-sm sm:text-xl flex flex-wrap gap-1">
+                        <span className="text-slate-500">Real</span>
+                        <span className="text-slate-700">World</span>
+                    </h1>
+                </Link>
+
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input type="text" placeholder="Search..." className="bg-transparent outline-none w-28 sm:w-64" />
+                    <FaSearch className='text-slate-600' />
+                </form>
+                <ul className='flex gap-4'>
+                    <Link to="/">
+                        <li className='hidden sm:inline font-semibold text-slate-600 hover:text-slate-800'>Home</li>
+
+                    </Link>
+                    <Link to="/about">
+                        <li className='hidden sm:inline font-semibold text-slate-600 hover:text-slate-800'>About</li>
+
+                    </Link>
+                    <Link to="/sign-in">
+                        <li className=' text-slate-600 font-semibold  hover:text-slate-800'>Sign In</li>
+
+                    </Link>
+
+                </ul>
+            </div>
+        </header>
     )
 }
 
