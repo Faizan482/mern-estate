9ae5d4a38991665828d3ce86a9ccdf9389f405f3
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-07-01 13:38:43 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-// import Home from './pages/Home'
-// import About from './pages/About'
-// import Profile from './pages/Profile'
-// import Signin from './pages/Signin'
-// import Signup from './pages/Signup'
-// import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
 
 const App = () => {
   return (
-    <h1 className="text-orange-500">hello</h1>
-    // <BrowserRouter>
-    //   {/* <Header /> */}
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/signin" element={< Signin />} />
-    //     <Route path="/signup" element={< Signup />} />
-    //     <Route path="/profile" element={< Profile />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
