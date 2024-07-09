a1802ee1dcf413756700bb92a3082054236dd9b6
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-07-09 22:17:41 +0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongooes connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
-mongoose.connect(process.env.MONGO);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/user", userRouter, (req, res) => {});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log("run the server on port 3000!!!");
 });
