36ca4389a86824f208bf2608c97637c07e1f214a
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-07-09 12:32:00 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongobd connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("run the server on port 3000!!!");
