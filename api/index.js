e90b572e3ff6ad0c76890162be5c147fadc811f3
muhammad-Faizan482 <155950643+muhammad-Faizan482@users.noreply.github.com>
2024-07-10 16:26:41 +0500

    create a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -18,6 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// make middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("run the server on port 3000!!!");
 });
